4eabfe998249ce3bdc1d62b0bfcacf463d146e1a
M0untainfox <21501216@etu.unicaen.fr>
2017-02-13 22:29:48 +0100

    Avec JSON :exclamation:   :exclamation:   :exclamation:
    Welcom JSON

--- Tp2/app.js
+++ Tp2/app.js
@@ -1,58 +1,34 @@
 (function(){
-  var dispoItems =[
-        {
-            "url": "http://tutorialzine.com/2013/07/50-must-have-plugins-for-extending-twitter-bootstrap/",
-            "title": "50 Must-have plugins for extending Twitter Bootstrap",
-            "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/07/featured_4-100x100.jpg"
-        },
-        {
-            "url": "http://tutorialzine.com/2013/08/simple-registration-system-php-mysql/",
-            "title": "Making a Super Simple Registration System With PHP and MySQL",
-            "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/08/simple_registration_system-100x100.jpg"
-        },
-        {
-            "url": "http://tutorialzine.com/2013/08/slideout-footer-css/",
-            "title": "Create a slide-out footer with this neat z-index trick",
-            "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/08/slide-out-footer-100x100.jpg"
-        },
-        {
-            "url": "http://tutorialzine.com/2013/06/digital-clock/",
-            "title": "How to Make a Digital Clock with jQuery and CSS3",
-            "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/06/digital_clock-100x100.jpg"
-        },
-        {
-            "url": "http://tutorialzine.com/2013/05/diagonal-fade-gallery/",
-            "title": "Smooth Diagonal Fade Gallery with CSS3 Transitions",
-            "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/05/featured-100x100.jpg"
-        },
-        {
-            "url": "http://tutorialzine.com/2013/05/mini-ajax-file-upload-form/",
-            "title": "Mini AJAX File Upload Form",
-            "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/05/ajax-file-upload-form-100x100.jpg"
-        },
-        {
-            "url": "http://tutorialzine.com/2013/04/services-chooser-backbone-js/",
-            "title": "Your First Backbone.js App – Service Chooser",
-            "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/04/service_chooser_form-100x100.jpg"
-        }
-    ];
+
 
   var myApp = angular.module('myApp', []);
 
-  myApp.controller("DispController",function(){
+  myApp.controller("DispController",function($scope, $http){
+
     var self = this;
 
-    this.Dispo = dispoItems;
 
     this.current = null;
 
     this.step=1;
 
+    this.dispoItems = null;
+
+
+
+    $http.get("data.json")
+    .then(function(response) {
+        $scope.var = response.data;
+        //alert($scope.myWelcome);
+        self.dispoItems = $scope.var;
+
+
+    });
 
 
 
     this.addToIncluded = function(Var){
-
+console.log(this.dispoItems[1]);
       for(i=0;i<Var.length;i++){
         Var[i].select=true;
       }
@@ -67,21 +43,21 @@
     };
 
     this.addAllToIncluded = function(){
-        for(i=0;i<this.Dispo.length;i++){
-          this.Dispo[i].select = true;
+        for(i=0;i<this.dispoItems.length;i++){
+          this.dispoItems[i].select = true;
         }
     };
 
     this.removeAllFromIncluded = function(){
-      for(i=0;i<this.Dispo.length;i++){
-        this.Dispo[i].select = false;
+      for(i=0;i<this.dispoItems.length;i++){
+        this.dispoItems[i].select = false;
       }
     };
 
     this.count = function(){
       res=0;
-      for(i=0;i<this.Dispo.length;i++){
-        if(this.Dispo[i].select == true){
+      for(i=0;i<this.dispoItems.length;i++){
+        if(this.dispoItems[i].select == true){
           res = res +1;
         }
 
